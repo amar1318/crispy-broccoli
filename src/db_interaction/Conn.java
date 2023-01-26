03757a048211c479c1f1ec9606503fba8c16940c
Amit <lakadeamit30email.com>
2023-01-26 21:06:32 +0530

    added connection class

--- src/db_interaction/Conn.java
+++ src/db_interaction/Conn.java
@@ -1,5 +1,20 @@
 package db_interaction;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 
 public class Conn {
-    
+    private static Connection connection;
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz","root","root");
+			
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+
+	}
 }
