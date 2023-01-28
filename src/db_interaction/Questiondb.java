7c2b90fa383f684654bbb608c076f75a0e70f5cf
Aniruddha Kamble <122866695+aniruddha06git@users.noreply.github.com>
2023-01-28 18:54:29 +0530

    Update Questiondb.java

--- src/db_interaction/Questiondb.java
+++ src/db_interaction/Questiondb.java
@@ -41,55 +41,6 @@ con.close();
 statement.close();
 				
 
-        } catch (Exception e) {
-          e.printStackTrace();  
-        }
-                     
-
-        Questions q1 = new Questions(" Which statement is true about Java?", "A. Java is a sequence-dependent programming language ", "B. Java is a code dependent programming language ", "C. Java is a platform-dependent programming language ", "D. Java is a platform-independent programming language ");
-        Questions q2 = new Questions("What is the extension of java code files?", " A. .txt", "B. .pdf", "C. .sql", "D. .java");
-        Questions q3 = new Questions("Who invented Java Programming? ", "A. Guido van Rossum", "B. James Gosling", "C. Dennis Ritchie", "D. Bjarne Stroustrup");
-        Questions q4 = new Questions("Which one of the following is not a Java feature?", "A. Object-oriented", "B. Use of pointers", "C. Portable", "D. Dynamic and Extensible");
-        Questions q5 = new Questions("Which of these cannot be used for a variable name in Java?", "A. identifier & keyword", "B. identifier", "C. Keyword", "D. none of the mentioned");
-                           
-
-
-        return questions;
-    }
-	 public class Questiondb2 {
-	PreparedStatement prs=null;
-	Connection con=null;
-	public void insertQuestions(String username,String score) {
-		 try {
-			 ConnectionTest connectionTest=new ConnectionTest();
-			 con=connectionTest.getConnectionDetails();
-			 prs=con.prepareStatement("insert into project__questiondb(username,score)values(?,?)");
-			 prs.setString(1, "username");
-			 prs.setString(2,score);
-			 int i=prs.executeUpdate();
-			 System.out.println("Done"+i);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	 }
-	
-	 public static void main(String[] args) throws SQLException {
-		 Scanner sc=new Scanner(System.in);
-		 for(int i=0;i<3;i++) {
-			 System.out.println("enter username");
-			 String userName=sc.next();
-			 System.out.println("enter score");
-			 String score=sc.next();
-			 
-			 Questiondb2 qs=new Questiondb2();
-			 qs.insertData(userName,score);
-		 }
-		 sc.close();
-	
-		 
-
-	}
 
 }
 
