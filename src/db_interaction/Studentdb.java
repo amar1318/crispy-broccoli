27c52d202e02f0c254ca08a9f6eeb7bf8b573a9a
Amar Gavade <85732640+amar1318@users.noreply.github.com>
2023-01-30 18:02:36 +0530

    Update Studentdb.java

--- src/db_interaction/Studentdb.java
+++ src/db_interaction/Studentdb.java
@@ -13,7 +13,7 @@ public class Studentdb {
     public static boolean addStudent(Student st){
         try {
              PreparedStatement ps2 = connection.prepareStatement("SELECT EXISTS (SELECT * FROM quiz WHERE student = ?) as 'check'");//sql query check exist user
-            ps2.setString(1, user.getid());
+            ps2.setString(1, st.getid());
             ResultSet rs2 = ps2.executeQuery();
             if(rs2.next()) {
                 if(rs2.getInt("check")==1){
@@ -21,9 +21,9 @@ public class Studentdb {
                 }
             }
             PreparedStatement ps = connection.prepareStatement("insert into student values(?,?,?)");//sql query add value
-            ps.setString(1, user.getPassword());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getScore());
+            ps.setString(1, st.getPassword());
+            ps.setString(2, st.getName());
+            ps.setString(3, st.getScore());
             ps.executeUpdate();
             
         } catch (Exception e) {
